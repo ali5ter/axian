c5a5d14934daa7a6df9ff978e34b2b3eb757bf28
Alister Lewis-Bowen <alister@different.com>
2014-03-03 15:13:56 -0500

    clean up source control of cordova generated files and add touchcontrols class

--- platforms/android/ant-build/Axian-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/alister/Documents/Projects/Personal/axian/platforms/android/ant-build/Axian-debug-unaligned.apk : \
-/Users/alister/Documents/Projects/Personal/axian/platforms/android/ant-build/Axian.ap_ \
-/Users/alister/Documents/Projects/Personal/axian/platforms/android/ant-build/classes.dex \
-/Users/alister/Documents/Projects/Personal/axian/platforms/android/CordovaLib/ant-build/classes.jar \
